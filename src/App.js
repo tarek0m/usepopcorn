84f5f94ddac05d9f1b653dafc9db8bd43207798b
TarekFoE <tarek.alnaggar@outlook.com>
2025-01-21 18:22:33 +0200

    Initial structure of the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
-function App() {
-  return <div>APP</div>;
-}
+import { useState } from 'react';
+import { tempMovieData } from './movieData.js';
+import NavBar from './components/NavBar.js';
+import Logo from './components/Logo';
+import Search from './components/Search';
+import NumResults from './components/NumResults';
+import Main from './components/Main.js';
+import Box from './components/Box.js';
+import MovieList from './components/MovieList';
+import WatchedSummary from './components/WatchedSummary.js';
+import WatchedMoviesList from './components/WatchedMoviesList.js';
+import { tempWatchedData } from './movieData.js';
 
-export default App;
+export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
+
+  return (
+    <>
+      <NavBar>
+        <Logo />
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
+
+      <Main>
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
+
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
+        </Box>
+      </Main>
+    </>
+  );
+}
---
+++ src/utils/average.js
@@ -0,0 +1,4 @@
+const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+export default average;
